d9d8f704991eb89e94703eb64d51ddc4ab007679
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-02-03 17:23:42 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,55 @@
+{
+    const formDefault = () => {
+        const form = document.querySelector('.form-js');
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+        });
 
-let inputLeftElement = document.querySelector('.form__input--left-js');
-let button = document.querySelector('.form__button-js');
-let form = document.querySelector('.form-js');
-let currencyLeftElement = document.querySelector('.form__select--left-js');
+    };
+
+    const documentObjects = () => {
+        const inputLeftElement = document.querySelector('.form__input--left-js');
+        const inputLeft = inputLeftElement.value;
+        const currencyLeftElement = document.querySelector('.form__select--left-js');
+        const firstInputValue = document.querySelector('.form__select--left-js');
+        const secondInputValue = document.querySelector('.form__select--right-js');
+        let result = document.querySelector('.form__span--result-js');
+        const values = {
+            pln: 1,
+            eur: 4,
+            usd: 4.5,
+            gbp: 5,
+            hkd: 8,
+            egp: 1.23
+        };
+        
+        const finalCount = (values[firstInputValue.value] / values[secondInputValue.value]) * inputLeft;
+        result.innerHTML = finalCount.toFixed(2);
+
+    };
+
+    const buttonFunction = () => {
+        const button = document.querySelector('.form__button-js');
+        formDefault()
+        button.addEventListener("click", documentObjects);
+
+    };
+
+    buttonFunction()
+
+}
 
 
 
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
+
+
+
+
+
+
+
+
 
 // Linijki do dopracowania
 // const formSpanInput = document.querySelector('.form__span--input');
@@ -24,29 +64,3 @@ form.addEventListener("submit", (event) => {
 //         formSpanInput.innerHTML = 'EUR'
 //     }
 // })
-console.log("patrzysz pewnie czy nie ma błędów co ?? ;)");
-    
-var values = {
-    pln: 1,
-    eur: 4,
-    usd: 4.5,
-    gbp: 5,
-    hkd: 8,
-    egp: 1.23
-};
-
-button.addEventListener("click", () => {
-    const inputLeft = inputLeftElement.value;
-    const result = document.querySelector('.form__span--result-js');
-    const firstInputValue = document.querySelector('.form__select--left-js');
-    const secondInputValue = document.querySelector('.form__select--right-js');
-
-    const finalCount = (values[firstInputValue.value] / values[secondInputValue.value]) * inputLeft;
-
-    result.innerHTML = finalCount.toFixed(2);
-
-});
-
-
-
-
